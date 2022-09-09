6440c1054013ad18e010605c2f242cb9df0c5777
stevenilo <stephenokafor80@yahoo.com>
2022-09-08 19:42:21 -0700

    alx c programming hello world task, c printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,6 +5,7 @@
 * this program prints 'with proper grammar, but the outcome is a piece of art'
 * return: 0
 */
+
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
