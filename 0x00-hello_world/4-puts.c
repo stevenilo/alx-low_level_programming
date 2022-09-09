f5be582e11703fa634e674ca193cb59d91acfea5
stevenilo <stephenokafor80@yahoo.com>
2022-09-08 19:11:05 -0700

    alx c programming task c print out

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 /**
-* main - print te string in the put function 
-*
+* main - print te string in the put function
 *
 * this program prints 'Programming is like building a multilingual puzzle' with puts function
 * Return: 0
 */
 int main(void)
 {
- 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
 }
