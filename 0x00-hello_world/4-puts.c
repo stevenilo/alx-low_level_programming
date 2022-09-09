338aa3752d9e53d79f416b10fa9a8b34cd7e75ed
stevenilo <stephenokafor80@yahoo.com>
2022-09-08 19:20:19 -0700

    alx c programming hello world task, c print out

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 
 /**
 * main - print te string in the put function
-* this program prints 'Programming is like building a multilingual puzzle' with puts function
+* this program prints 'Programming is like building a multilingual puzzle'
 * Return: 0
 */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
