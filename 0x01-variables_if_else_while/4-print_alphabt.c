05dc42c0c4e27529cabe524301f1bf033def99c6
stevenilo <stephenokafor80@yahoo.com>
2022-09-09 20:04:56 -0700

    ALX c programming c variable, if else, while task

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main - print the alphabet in lowercase followedby a new lin
+ *using the main function
+ *Retrun: 0
+ */
+
+int main(void)
+{
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+if (ch != 'e' && ch != 'q')
+{
+putchar(ch);
+}
+}
+putchar('\n');
+return (0);
+}
