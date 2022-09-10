829afa6420fdac5f515231026361b9622e8a5db4
stevenilo <stephenokafor80@yahoo.com>
2022-09-09 20:25:27 -0700

    ALX c programming c variable, if else, while task

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - print alphabets in reversal (lowercase)
+*using main function
+*Return: 0
+*/
+
+int main(void)
+{
+char c;
+for (c = 'z'; c >= 'a'; c--)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
