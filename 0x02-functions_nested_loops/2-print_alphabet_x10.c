7b90e417d91e5c73f67cc0644f32f63be6a30757
stevenilo <stephenokafor80@yahoo.com>
2022-09-13 15:47:39 -0700

    ALX C PROGRAMMING function nested task

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - function to print abc 10 times
+ *
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+char s, p;
+
+for (s = 0; s <= 9; i++);
+{
+for (p = 'a'; p <= 'z'; p++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
+}
