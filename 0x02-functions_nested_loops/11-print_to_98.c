519611d2d25a1199d3ec05da09b593e76bca1b80
stevenilo <stephenokafor80@yahoo.com>
2022-09-13 19:13:18 -0700

    ALX C Programming function nested task

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * print_to_98 - prints  all natural numbers from input to 98,
+ * in order seperated by a comma followed by a space.
+ * @n: the number to begincounting at.
+ */
+
+void print_to_98(int n)
+{
+if (n >= 98)
+{
+while (n > 98)
+printf("%d, ", n--);
+printf("%d\n", n);
+}
+else
+{
+while (n < 98)
+printf("%d, ", n++);
+printf("%d\n", n);
+}
+}
