b9ba1c38e9b445789e94de55cca81647f8a1d05d
stevenilo <stephenokafor80@yahoo.com>
2022-09-15 15:50:41 -0700

    ALX C Programming more functions nested loops task

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mult - multiplies two numbers
+ * @a: first number
+ * @b: second number
+ * Return: returns result
+ */
+
+int mult(int a, int b)
+{
+return (a * b);
+}
