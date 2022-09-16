cd0f6308857729ec72151c7b52bbbec24a684ba5
stevenilo <stephenokafor80@yahoo.com>
2022-09-15 19:00:27 -0700

    ALX C Programming more function nested loops task

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number 1-100 but multiples of 3 print
+ *      Fizz and Buzz for thr multiples of 5 and Fizzbuzz
+ *      For both 5 and 3
+ *
+ * Return: Always 0;
+ */
+
+int main(void)
+{
+int n;
+
+for (n = 1, n <= 100; n++)
+{
+if ((n % 3) == 0 && (n % 5) == 0)
+{
+printf("FizzBuzz");
+}
+else if ((n % 3) == 0)
+{
+printf("Fizz");
+}
+else if ((n % 5) == 0)
+{
+printf("Buzz");
+}
+else
+{
+printf("%d", n);
+}
+if (n == 100)
+{
+continue;
+}
+printf(" ");
+}
+printf("\n");
+return (0);
+}
