3b8bb84a74c172c9d321921ba5993d2972fe3185
stevenilo <stephenokafor80@yahoo.com>
2022-09-19 14:26:33 -0700

    ALX C Programming pointer arrays and strings task

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -13,9 +13,9 @@ void _puts(char *str)
 {
 int i = 0;
 
-while (*(str + 1) != '\0')
+while (*(str + i) != '\0')
 {
-putchar(*(str + 1));
+putchar(*(str + i));
 i++;
 }
 putchar(10);
--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * puts2 - prints every other character 
+ * puts2 - prints every other character
  * @str: string
  *
  *Return: nothing
