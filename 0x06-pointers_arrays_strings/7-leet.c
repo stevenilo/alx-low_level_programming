1792aa2afc66f46054d913ddce3a4fc278fb7860
stevenilo <stephenokafor80@yahoo.com>
2022-09-21 17:55:24 -0700

    ALX C Programming more pointer arrays and strings tasks

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * leet - leet encoder
+ * leet - leet encoder to 1337.
  * @s: input string
  *
  * Return: pointer to dest
@@ -11,7 +11,21 @@
 char *leet(char *s)
 {
 
-  int count = 0, i;
-  
-  return (s);
+int i, ii;
+char s1[] = "aeotl";
+char t1[] = "AEOTL";
+char s2[] = "43071";
+
+for (i = 0; s[i] != '\0'; i++)
+{
+for (ii = 0; ii < 5; ii++)
+{
+if (s[i] == s1[ii] || s[i] == t1[ii])
+{
+s[i] = s2[ii];
+break;
+}
+}
+}
+return (s);
 }
