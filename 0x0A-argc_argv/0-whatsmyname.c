42211156743083d2926fa412feeec68d2cd11643
stevenilo <stephenokafor80@yahoo.com>
2022-09-30 17:01:59 -0700

    ALX C Programming argc and argv tasks

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Write a program that prints its name, followed by a new line.
+ * if you rename the program, it will print the new name, without having to
+ * compile it again
+ * you should not remove the path before the name of the program
+ *
+ * @argc: This is the argument count
+ * @argv: This is the argument vector
+ *
+ * Return: This return to 0
+ */
+
+int main(int argc, char *argv[])
+{
+  printf("%s\n", argv[]);
+  (void)argc;
+
+  return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+ALX C Porgramming C Argc, agrv task
