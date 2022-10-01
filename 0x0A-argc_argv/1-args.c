b37c9e8d9b9ed8b72030f3ddc3e87b37cae6fe51
stevenilo <stephenokafor80@yahoo.com>
2022-09-30 17:15:27 -0700

    ALX C Programming argc and argv task

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - write a program that prints the number of arguments passed into it
+ *
+ * @argc: this is the argument count
+ * @argv: this is the argument vector
+ *
+ * Return: this return to 0
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+printf("%d\n", argc - 1);
+(void)argv;
+return (0);
+}
