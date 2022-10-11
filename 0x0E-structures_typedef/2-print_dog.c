686772a9847a65f2e0f538a5b4f86c7100cc8e5b
stevenilo <stephenokafor80@yahoo.com>
2022-10-10 18:50:33 -0700

    ALX C Programming C structures, typedef tasks

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,4 +1,4 @@
-#include "dog.h
+#include "dog.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -11,7 +11,7 @@ void print_dog(struct dog *d)
 {
 if (d != NULL)
 {
-printf("Name : % s\nAge : % f\nOwner : % s\n",
+printf("Name: %s\nAge: %f\nOwner: %s\n",
 (*d).name != NULL ? d->name : "(nil)",
 (*d).age,
 (*d).owner != NULL ? d->owner : "(nil)");
