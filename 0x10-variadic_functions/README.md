447355a81cdb38dd6ebeb83c4ed2ed3d32f10e3c
stevenilo <stephenokafor80@yahoo.com>
2022-10-13 19:19:11 -0700

    ALX C Programming variadic functions tasks

---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+ALX C Programming Variadic function tasks
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,19 @@
+#ifndef _VARIADIC_FUNCTIONS_H_
+#define _VARIADIC_FUNCTIONS_H_
+
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * struct print - print type with correspoinding print function
+ * @t: print type
+ * @f: print function
+ */
+
+int _putchar(char);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
