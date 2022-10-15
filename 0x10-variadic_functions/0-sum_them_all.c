2ede0723fdc6425dc5314eb670c5f988c195e966
stevenilo <stephenokafor80@yahoo.com>
2022-10-14 18:07:44 -0700

    ALX C Programming variadic function tasks

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,14 +10,17 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-int sum = 0;
-unsigned int i;
 va_list valist;
+unsigned int i;
+int sum = 0;
+
+if (n == 0)
+return (0);
 
 va_start(valist, n);
 for (i = 0; i < n; i++)
 sum += va_arg(valist, int);
 va_end(valist);
 
-return (2);
+return (sum);
 }
