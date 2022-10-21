702c7870496a89bb364ee17f4b8005b46cc444c1
stevenilo <stephenokafor80@yahoo.com>
2022-10-20 20:53:53 -0700

    ALX C Programming singly linked list task

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,28 +3,28 @@
 
 /**
  * add_node - adds a new node at the beginning
- *   of a list_t list.
- * @head: a pointer to the head of the list_t list
- * @str: the string to be added to the list_t list
- *
- * Return: if the function fails - NULL
- *    otherwise - the address of the new element
+ * of a list_t list.
+ * @head: head of the linked list.
+ * @str: string to store in the list.
+ * Return: address of the head.
  */
 
 list_t *add_node(list_t **head, const char *str)
 {
-char *dup;
-int len;
-list_t *new;
+list_t *new_node;
+size_t n;
 
-new = malloc(sizeof(list_t));
-if (new == NULL)
+new_node = malloc(sizeof(list_t));
+if (new_node == NULL)
 return (NULL);
 
-dup = strdup(str);
-if (dup == NULL)
-{
-free(new);
-return (NULL);
-}
+new_node->str = strdup(str);
+for (n = 0; str[n]; n++)
+;
+new_node->len = n;
+new_node->next = *head;
+*head = new_node;
+
+return (*head);
+
 }
