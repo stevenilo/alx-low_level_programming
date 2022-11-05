7dae590e0158d42a3388fa22e8a4db173eb67614
stevenilo <stephenokafor80@yahoo.com>
2022-11-04 23:05:34 -0700

    ALX C Programming File I/O tasks

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,39 +1,55 @@
 #include "main.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <stdlib.h>
 
 /**
- * read_textfile - Reads a text file and prints it to POSIX stdout
- * @filename: a pointer to the name of the file
- * @letters: the number of letters the function should read and print
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: name of the file to read
+ * @letters: number of letters it should read and print
  *
- * Return: If the function fails or filename is NULL - 0
- *        O/W - the actual number of bytes the function can read and print
+ * Return: actual number of letters it could read and print
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-ssize_t o, r, w;
+int fd;
+ssize_t lenr, lenw;
 char *buffer;
 
 if (filename == NULL)
 return (0);
 
-buffer = malloc(sizeof(char) * letters);
-if (buffer == NULL)
+fd = open(filename, O_RDONLY);
+if (fd == -1)
 return (0);
 
-o = open(filename, O_RDONLY);
-r = read(o, buffer, letters);
-w = write(STDOUT_FILENO, buffer, r);
+buffer = malloc(sizeof(char) * letters);
 
-if (0 == -1 || r == -1 || w == -1 || w != r)
+if (buffer == NULL)
+{
+close(fd);
+return (0);
+}
+
+lenr = read(fd, buffer, letters);
+
+close(fd);
+
+if (lenr == -1)
 {
 free(buffer);
 return (0);
 }
 
+lenw = write(STDOUT_FILENO, buffer, lenr);
 free(buffer);
-close(0);
 
-return (w);
+if (lenr != lenw)
+
+return (0);
+
+return (lenw);
 }
